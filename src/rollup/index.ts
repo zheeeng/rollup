4f70e5b884ecc42a0e6ed8016303f7766c797a59
Guy Bedford <guybedford@gmail.com>
2018-03-27 00:47:02 +0200

    complete tests, optimizeChunks option, dont merge manual chunks

--- src/rollup/index.ts
+++ src/rollup/index.ts
@@ -112,6 +112,7 @@ export interface InputOptions {
 	experimentalCodeSplitting?: boolean;
 	preserveSymlinks?: boolean;
 	experimentalPreserveModules?: boolean;
+	optimizeChunks?: boolean;
 	chunkGroupingSize?: number;
 
 	// undocumented?
@@ -470,6 +471,9 @@ export default function rollup(
 			.then(chunks => {
 				timeEnd('BUILD', 1);
 
+				// ensure we only do one optimization pass per build
+				let optimized = false;
+
 				function generate(rawOutputOptions: GenericConfigObject) {
 					const outputOptions = normalizeOutputOptions(inputOptions, rawOutputOptions);
 
@@ -495,7 +499,6 @@ export default function rollup(
 					if (inputOptions.experimentalPreserveModules)
 						preserveModulesBase = commondir(chunks.map(chunk => chunk.entryModule.id));
 					let existingNames = Object.create(null);
-					let optimizedChunks = chunks;
 
 					const promise = createAddons(graph, outputOptions)
 						.then(addons => {
@@ -512,12 +515,13 @@ export default function rollup(
 										for (let chunk of chunks) {
 											chunk.preRender(outputOptions);
 										}
-										if (!inputOptions.experimentalPreserveModules) {
-											optimizedChunks = optimizeChunks(
-												chunks,
-												outputOptions,
-												inputOptions.chunkGroupingSize
-											);
+										if (
+											!optimized &&
+											!inputOptions.experimentalPreserveModules &&
+											inputOptions.optimizeChunks
+										) {
+											optimizeChunks(chunks, outputOptions, inputOptions.chunkGroupingSize);
+											optimized = true;
 										}
 										for (let chunk of chunks) {
 											if (inputOptions.experimentalPreserveModules) {
@@ -536,7 +540,7 @@ export default function rollup(
 									// second render chunks given known names
 									.then(() => {
 										return Promise.all(
-											optimizedChunks.map(chunk => {
+											chunks.map(chunk => {
 												return chunk.render(outputOptions, addons).then(rendered => {
 													const output = {
 														imports: chunk.getImportIds(),
